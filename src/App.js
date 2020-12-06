c618e7cfed30b24ce18932a7cd1f9b0f3dbe1c02
KshitijBahul <chunks.3@gmail.com>
2020-12-06 20:52:07 +0100

    Using SetState to change the state

--- src/App.js
+++ src/App.js
@@ -16,16 +16,11 @@ class App extends Component{
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
             {this.state.string}
-          </a>
+          </p>
+          <button onClick = {()=> this.setState({string : 'Hello Kshitij' })} >
+            Click Me to change !
+          </button>
         </header>
       </div>
     );
